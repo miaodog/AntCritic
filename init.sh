1c8967fc6c56202d5b5f3360e151f64a1ff877c6
content-tech <content-tech@service.alipay.com>
2022-09-15 15:54:25 +0800

    track antcritic/train.1.csv files using Git LFS

--- init.sh
+++
@@ -1,4 +0,0 @@
-source /mnt/fengyao.hjj/miniconda3/bin/activate; conda activate antmmf; cuda101=/gruntdata/DL_dataset/qingpei.gqp/software/cuda-10.1; export PATH=${cuda101}/bin:${cuda101}/lib64:$PATH; export LD_LIBRARY_PATH=${cuda101}/lib64:${cuda101}/extras/CUPTI/lib64:$LD_LIBRARY_PATH; cd /mnt/workspace/wenyu.zy/ar*
-
-source /mnt/yugong.xwq/miniconda3/bin/activate
-conda activate antmmf
