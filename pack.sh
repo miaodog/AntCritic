454fd97ebd92b26c88138c26c263e48d8926591f
凤瑶 <huojingjing.hjj@antgroup.com>
2022-09-15 15:26:27 +0800

    publish code on github

--- pack.sh
+++
@@ -1,6 +0,0 @@
-cd m2-title-generation
-mkdir -p transformers
-cp /Users/fengyao/PycharmProjects/transformers/cmodels transformers/
-cp /Users/fengyao/PycharmProjects/transformers/src transformers/
-cd ../
-tar zcvf m2-title-generation-0419-v1.tar.gz m2-title-generation
